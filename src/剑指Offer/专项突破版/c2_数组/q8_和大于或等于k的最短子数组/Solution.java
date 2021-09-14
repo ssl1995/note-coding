190ad3df7388e9584d8ab79c7967d204fabc0837
songshenglin <872912679@qq.com>
2021-09-14 09:41:12 +0800

    和>=k的最短子数组、乘积<k的子数组

---
+++ src/剑指Offer/专项突破版/c2_数组/q8_和大于或等于k的最短子数组/Solution.java
@@ -0,0 +1,24 @@
+package 剑指Offer.专项突破版.c2_数组.q8_和大于或等于k的最短子数组;
+
+/**
+ * @author SongShengLin
+ * @date 2021/9/14 9:04 上午
+ */
+public class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;// left、right初始化指向数组第一位
+        int sum = 0;// 记录和
+        int minLen = Integer.MAX_VALUE;// minLen：记录最短距离
+        for (int right = 0; right < nums.length; right++) {// sum<t，right右移
+            sum += nums[right];
+            // 判断是否需要右移left：sum>=t，说明sum太大了，右移left
+            while (left <= right && sum >= target) {
+                // minLen：记录right和left的距离最小值
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        // minLen如果距离值没变，那就是没有找到>=k的sum，返回0
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+}
