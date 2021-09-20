13232ca79110975a651b5b7e80a45d5966fe1913
ssl1995 <872912679@qq.com>
2021-09-20 17:11:39 +0800

    有效的回文、最多删除一个字符得到回文、回文子字符串的个数

---
+++ src/力扣/剑指Offer/第三版/第3章_字符串/q18_有效的回文/Solution.java
@@ -0,0 +1,26 @@
+package 力扣.剑指Offer.第三版.第3章_字符串.q18_有效的回文;
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(j);
+            if (!Character.isLetterOrDigit(c1)) {
+                i++;
+            } else if (!Character.isLetterOrDigit(c2)) {
+                j--;
+            } else {
+                c1 = Character.toLowerCase(c1);
+                c2 = Character.toLowerCase(c2);
+                if (c1 != c2) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
