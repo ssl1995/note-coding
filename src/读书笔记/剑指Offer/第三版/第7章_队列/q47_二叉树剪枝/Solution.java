f5cb48c8ce84b95ff2de5a1239ab35313ed9d2a5
songshenglin <872912679@qq.com>
2021-10-09 19:50:32 +0800

    q47_二叉树剪枝

---
+++ src/读书笔记/剑指Offer/第三版/第7章_队列/q47_二叉树剪枝/Solution.java
@@ -0,0 +1,23 @@
+package 读书笔记.剑指Offer.第三版.第7章_队列.q47_二叉树剪枝;
+
+/**
+ * @author SongShengLin
+ * @date 2021/10/9 7:42 下午
+ * @description
+ */
+public class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return  null;
+        }
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+
+        return root;
+    }
+}
