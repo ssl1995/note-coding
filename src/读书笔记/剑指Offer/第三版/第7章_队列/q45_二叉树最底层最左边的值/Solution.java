a4092071b985b50d9bb00379590d4249f0f85764
songshenglin <872912679@qq.com>
2021-10-09 15:15:02 +0800

    q45_二叉树最底层最左边的值；q46_二叉树的右侧视图

---
+++ src/读书笔记/剑指Offer/第三版/第7章_队列/q45_二叉树最底层最左边的值/Solution.java
@@ -0,0 +1,44 @@
+package 读书笔记.剑指Offer.第三版.第7章_队列.q45_二叉树最底层最左边的值;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author SongShengLin
+ * @date 2021/10/9 2:49 下午
+ * @description
+ */
+public class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        queue1.offer(root);
+        // 一开始bottomLeft就要更新根节点的值
+        int bottomLeft = root.val;
+
+        while (!queue1.isEmpty()) {
+            TreeNode node = queue1.poll();
+
+            if (node.left != null) {
+                queue2.offer(node.left);
+            }
+            if (node.right != null) {
+                queue2.offer(node.right);
+            }
+
+            if (queue1.isEmpty()) {
+                queue1 = queue2;
+                queue2 = new LinkedList<>();
+                if (!queue1.isEmpty()) {
+                    // 队列的peek是队列最左边的值
+                    bottomLeft = queue1.peek().val;
+                }
+            }
+
+        }
+        return bottomLeft;
+    }
+}
---
+++ src/读书笔记/剑指Offer/第三版/第7章_队列/q45_二叉树最底层最左边的值/TreeNode.java
@@ -0,0 +1,25 @@
+package 读书笔记.剑指Offer.第三版.第7章_队列.q45_二叉树最底层最左边的值;
+
+/**
+ * @author SongShengLin
+ * @date 2021/10/8 9:20 上午
+ * @description
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
