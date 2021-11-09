41cb59ffa7ae68fb646c80c73ae2864ce329599c
songshenglin <872912679@qq.com>
2021-11-09 23:19:40 +0800

    q78_合并排序链表

---
+++ src/读书笔记/剑指Offer/第三版/第12章_排序/q78_合并排序链表/ListNode.java
@@ -0,0 +1,23 @@
+package 读书笔记.剑指Offer.第三版.第12章_排序.q78_合并排序链表;
+
+/**
+ * @author SongShengLin
+ * @date 2021/11/9 9:14 上午
+ * @description
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
