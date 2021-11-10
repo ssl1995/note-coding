6f952ef3e60cc4b615f81da380823062361611a5
songshenglin <872912679@qq.com>
2021-11-10 09:48:33 +0800

    q81_允许重复选择元素的组合

---
+++ src/读书笔记/剑指Offer/第三版/第12章_排序/q81_允许重复选择元素的组合/Solution.java
@@ -0,0 +1,34 @@
+package 读书笔记.剑指Offer.第三版.第12章_排序.q81_允许重复选择元素的组合;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author SongShengLin
+ * @date 2021/11/10 9:40 上午
+ * @description
+ */
+public class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        LinkedList<Integer> com = new LinkedList<>();
+        helper(candidates, target, 0, com, res);
+        return res;
+    }
+
+    private void helper(int[] nums, int target, int index, LinkedList<Integer> com, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new LinkedList<>(com));
+        } else if (target > 0 && index < nums.length) {
+            // 选择1：没有选择当前数字，下标+1递归
+            helper(nums, target, index + 1, com, res);
+
+            // 选择2：选择当前数字，由于结果集可以有重复数字，所以下标不变递归
+            com.add(nums[index]);
+            helper(nums, target - nums[index], index, com, res);
+            // 回到父节点时，清除子集状态
+            com.removeLast();
+        }
+    }
+}
