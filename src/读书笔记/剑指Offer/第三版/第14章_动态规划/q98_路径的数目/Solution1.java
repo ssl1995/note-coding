26a1c1cf5a0d938aad53419c5d52ed26dcd2a09a
songshenglin <872912679@qq.com>
2021-11-20 23:08:53 +0800

    q97_子序列的数目

---
+++ src/读书笔记/剑指Offer/第三版/第14章_动态规划/q98_路径的数目/Solution1.java
@@ -0,0 +1,33 @@
+package 读书笔记.剑指Offer.第三版.第14章_动态规划.q98_路径的数目;
+
+/**
+ * @author SongShengLin
+ * @date 2021/11/20 10:45 下午
+ * @description
+ */
+public class Solution1 {
+
+    public int uniquePaths(int m, int n) {
+        // dp[i][j]表示从0点到该坐标的路径数
+        int[][] dp = new int[m][n];
+        // 初始化第一行和第一列，只有一种路径到达
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        // 转移方程
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution1 solution = new Solution1();
+        System.out.println(solution.uniquePaths(3, 3));
+    }
+}
---
+++ src/读书笔记/剑指Offer/第三版/第14章_动态规划/q98_路径的数目/Solution2.java
@@ -0,0 +1,28 @@
+package 读书笔记.剑指Offer.第三版.第14章_动态规划.q98_路径的数目;
+
+/**
+ * @author SongShengLin
+ * @date 2021/11/20 10:45 下午
+ * @description
+ */
+public class Solution2 {
+
+    public int uniquePaths(int m, int n) {
+        // 优化空间，只用一维数组
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        System.out.println(solution.uniquePaths(3, 3));
+    }
+}
