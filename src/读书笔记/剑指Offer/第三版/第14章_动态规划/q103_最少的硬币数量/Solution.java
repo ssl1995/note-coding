2c46ab2a9e3a20d33f4d41692d97539e30734634
songshenglin <872912679@qq.com>
2021-11-23 09:59:10 +0800

    q103_最少的硬币数量

---
+++ src/读书笔记/剑指Offer/第三版/第14章_动态规划/q103_最少的硬币数量/Solution.java
@@ -0,0 +1,28 @@
+package 读书笔记.剑指Offer.第三版.第14章_动态规划.q103_最少的硬币数量;
+
+import java.util.Arrays;
+
+/**
+ * @author SongShengLin
+ * @date 2021/11/23 9:30 上午
+ * @description
+ */
+public class Solution {
+    // coins是正整数
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        // amount + 1表示某个面额不能用输入的硬币凑出
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int j = amount; j >= 1; j--) {
+                for (int k = 1; k * coin <= j; k++) {
+                    dp[j] = Math.min(dp[j], dp[j - k * coin] + k);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
