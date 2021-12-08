57f89a825e3869055353ac6619a435574e77f47e
songshenglin <872912679@qq.com>
2021-12-08 09:48:19 +0800

    q10_II_青蛙跳台阶

--- src/剑指Offer/第二版/第2章_面试需要的基础知识/q10_II_青蛙跳台阶/Solution.java
+++ src/剑指Offer/第二版/第2章_面试需要的基础知识/q10_II_青蛙跳台阶/Solution.java
@@ -4,9 +4,12 @@ public class Solution {
 
     // 一次可以跳1个台阶或者2个台阶
     public int numWays(int n) {
+        // 青蛙跳台阶，第一个台阶需要1次，第二个台阶需要2次
+        // 隐式的告诉我们假设有第0个台阶，它也需要第跳1次，才能满足f(n)=f(n-1)+f(n-2)
         if (n == 0 || n == 1) {
             return 1;
         }
+        // 由于前两项是f(0),f(1)都等于1，这是和斐波那契的区别
         int a = 1;
         int b = 1;
         int sum = 0;
