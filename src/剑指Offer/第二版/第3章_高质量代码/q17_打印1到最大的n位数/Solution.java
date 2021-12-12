03c55ebb26fbe19e1bd4babba3b0758459f6c803
songshenglin <872912679@qq.com>
2021-12-12 11:06:23 +0800

    q17_打印1到最大的n位数,修改注释

--- src/剑指Offer/第二版/第3章_高质量代码/q17_打印1到最大的n位数/Solution.java
+++ src/剑指Offer/第二版/第3章_高质量代码/q17_打印1到最大的n位数/Solution.java
@@ -1,83 +1,66 @@
 package 剑指Offer.第二版.第3章_高质量代码.q17_打印1到最大的n位数;
 
+import java.util.Arrays;
+
 public class Solution {
-    // 方法1：自己算边界值
-    public int[] printNumbers1(int n) {
-        // 计算出最大边界
-        int boundary = 1;
-        for (int i = 1; i <= n; i++) {
-            // 每次乘以10
-            boundary *= 10;
-        }
-        // 定义一个返回值结果
-        int[] res = new int[boundary - 1];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = i + 1;
-        }
-        return res;
-    }
 
-    // 方法2:使用库函数计算边界值
-    public int[] printNumbers2(int n) {
-        // 10^n-1是n位数最大值 = 数组长度
-        int[] res = new int[(int) Math.pow(10, n) - 1];
-        for (int i = 0; i < res.length; i++) {
-            // 数组元素比索引大1,因为数是从1开始的
-            res[i] = i + 1;
-        }
-        return res;
-    }
-
-    // 方法3:字符串自增,返回给int
+    /**
+     * 打印从1到n位数的最大值，比如n=3，3位数最大值为999，则打印1，2，3到999
+     * 方法：自己计算n位数，但是n很大时，n位数可能会越界；考虑用字符串表示大数
+     */
     public int[] printNumbers(int n) {
+        // n位数最大值：(int) Math.pow(10, n) - 1
         int[] res = new int[(int) Math.pow(10, n) - 1];
+        // 遍历res的指针
+        int index = 0;
+
         StringBuilder sb = new StringBuilder();
-        // sb中往后添加n个0
+        // 初始化字符串的每一位都为"0"
         for (int i = 0; i < n; i++) {
             sb.append("0");
         }
-        int resIndex = 0;
-        // 没有发生进位就将字符串的值放回数组
-        while (!addOne(sb)) {
+        while (!strInsertOneAndIsPassMax(sb)) {
             // 先删除字符串前面多余的0,找到非0的坐标
-            int sbIndex = 0;
-            while (sbIndex < sb.length() && sb.charAt(sbIndex) == '0') {
-                sbIndex++;
+            int sbStart = 0;
+            while (sbStart < sb.length() && sb.charAt(sbStart) == '0') {
+                sbStart++;
             }
             // 非0元素赋值回原数组
-            res[resIndex] = Integer.parseInt(sb.substring(sbIndex));
-            resIndex++;
+            res[index++] = Integer.parseInt(sb.substring(sbStart));
         }
         return res;
     }
 
-    // 字符串+1,返回值是判断是否产生进位
-    private boolean addOne(StringBuilder sb) {
-        // 是否超过最大值
-        boolean passMax = false;
-        // 从字符串最高位开始往前遍历
-        for (int index = sb.length() - 1; index >= 0; index--) {
-            char addOneChar = (char) (sb.charAt(index) + 1);
-            if (addOneChar > '9') {
+    /**
+     * 将字符串表示的大数+1，并返回是否产生进位
+     */
+    private boolean strInsertOneAndIsPassMax(StringBuilder sb) {
+        // 是否越过字符串表示大数的最大值
+        boolean isPassMax = false;
+        // 字符串的len-1位=对应大数的最低位，依次开始遍历
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            char ch = (char) (sb.charAt(i) + 1);
+            if (ch > '9') {
                 // 进位后已经超过9,就将原位置替换为0
-                sb.replace(index, index + 1, "0");
-                if (index == 0) {
-                    passMax = true;
+                // replace：[start,end)取不到end位
+                sb.replace(i, i + 1, "0");
+                if (i == 0) {
+                    isPassMax = true;
                 }
             } else {
-                sb.replace(index, index + 1, String.valueOf(addOneChar));
+                sb.replace(i, i + 1, String.valueOf(ch));
                 // 没有超过9的自增直接break返回
                 break;
             }
         }
-        return passMax;
+        return isPassMax;
     }
 
     public static void main(String[] args) {
 //        StringBuilder str = new StringBuilder("001");
 //        str.replace(0, 1, "2");
-//        System.out.println(str.toString());
+//        System.out.println(str);
         Solution solution = new Solution();
-        solution.printNumbers(1);
+        System.out.println(Arrays.toString(solution.printNumbers(2)));
     }
 }
