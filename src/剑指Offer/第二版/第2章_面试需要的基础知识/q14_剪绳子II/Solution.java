eb7ff53f920fa5be6944bce324705e336e8935c4
songshenglin <872912679@qq.com>
2021-12-12 10:08:29 +0800

    q14_剪绳子II,封装循环取余法

--- src/剑指Offer/第二版/第2章_面试需要的基础知识/q14_剪绳子II/Solution.java
+++ src/剑指Offer/第二版/第2章_面试需要的基础知识/q14_剪绳子II/Solution.java
@@ -14,14 +14,11 @@ public class Solution {
         // 取a中3个个数-1，因为后面rem*3等，留了一个空位出来
         int a = n / 3 - 1;
         int b = n % 3;
-        long rem = 1;
         int x = 3;
         // 题目规定的取余数
         int p = 1000000007;
         // 循环求余法
-        for (int i = 1; i <= a; i++) {
-            rem = (rem * x) % p;
-        }
+        long rem = reminder(x, a, p);
         // 剩下和剪绳子I类似啦，将I中的pow换成rem即可
         if (b == 0) {
             return (int) (rem * (3) % p);
@@ -36,10 +33,10 @@ public class Solution {
     /**
      * 循环求余法：(x^a)%p的余数=rem
      */
-    private long reminder(int x, int a, int standard) {
+    private long reminder(int x, int a, int p) {
         long rem = 1;
         for (int i = 1; i <= a; i++) {
-            rem = (rem * x) % standard;
+            rem = (rem * x) % p;
         }
         return rem;
     }
--- src/剑指Offer/第二版/第3章_高质量代码/q16_数值的整数次方/Solution.java
+++ src/剑指Offer/第二版/第3章_高质量代码/q16_数值的整数次方/Solution.java
@@ -2,6 +2,10 @@ package 剑指Offer.第二版.第3章_高质量代码.q16_数值的整数次方;
 
 public class Solution {
 
+    /**
+     * 求x^n方，考虑n可能是负数、0、正数
+     * 快速幂法：思考x^9=x^1001=x^(1*1+0*2+0*4+1*8)推导
+     */
     public double myPow(double x, int n) {
         if (x == 0) {
             return 0.0;
@@ -17,10 +21,13 @@ public class Solution {
             b = -b;
         }
         double res = 1.0;
-        while (b > 0) {// 这里b=|b|,循环条件>0即可；如果b能为负数，循环条件是b!=0
-            if ((b & 1) == 1) {// bi=1时，res乘xi
+        // 这里b=|b|,循环条件>0即可；如果b能为负数，循环条件是b!=0
+        while (b > 0) {
+            // 依次判断指数的二进制最后一位是1or0
+            // 是1，就需要乘x的倍数；是0，就无须乘
+            if ((b & 1) == 1) {
                 res *= x;
-            } else {// bi=0时,res乘1
+            } else {
                 res *= 1;
             }
             // b有多少位二进制数，x就乘多少次
