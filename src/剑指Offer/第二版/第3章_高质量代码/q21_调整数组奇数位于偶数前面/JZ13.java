03cdf1d6a5194bbfc30c69371230014085f3582e
songshenglin <872912679@qq.com>
2021-12-14 10:24:58 +0800

    q21_调整数组奇数位于偶数前面，修改注释

--- src/剑指Offer/第二版/第3章_高质量代码/q21_调整数组奇数位于偶数前面/JZ13.java
+++ src/剑指Offer/第二版/第3章_高质量代码/q21_调整数组奇数位于偶数前面/JZ13.java
@@ -1,7 +1,9 @@
 package 剑指Offer.第二版.第3章_高质量代码.q21_调整数组奇数位于偶数前面;
 
 public class JZ13 {
-    // 牛客：保持相对次序不变，与力扣不一样
+    /**
+     * 牛客：保持相对次序不变，与力扣不一样
+     */
     public int[] reOrderArray(int[] array) {
         int[] res = new int[array.length];
         int index = 0;
