a817afe8814f80026ac5f8c283bab94319cf0111
songshenglin <872912679@qq.com>
2021-12-16 09:08:30 +0800

    q26_树的子结构，修改注释

--- src/剑指Offer/第二版/第3章_高质量代码/q26_树的子结构/Solution.java
+++ src/剑指Offer/第二版/第3章_高质量代码/q26_树的子结构/Solution.java
@@ -5,16 +5,21 @@ import 剑指Offer.utils.TreeNode;
 
 public class Solution {
 
+    /**
+     * 判断B是否是A的子树
+     */
     public boolean isSubStructure(TreeNode A, TreeNode B) {
         // base case:空节点，返回false
         if (A == null || B == null) {
             return false;
         }
-        // 先判断A的先序序列中是否用B || 如A中先序有B，则isContainB判断左右子树能否一一对应
+        // B是A的子结构||以A为根结点包含B
         return isSubStructure(A.left, B) || isSubStructure(A.right, B) || isContainB(A, B);
     }
 
-    // 判断A中以root为根节点的树，是否能匹配上match
+    /**
+     * 判断A中以root为根节点的树，是否能匹配上match
+     */
     private boolean isContainB(TreeNode root, TreeNode match) {
         // base1：match越过叶子，说明匹配完成，返回true
         if (match == null) {
