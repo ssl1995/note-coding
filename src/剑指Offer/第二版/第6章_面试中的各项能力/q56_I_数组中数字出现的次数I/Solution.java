8958e51607f0cf199d76b460d8289794c5891ca9
songshenglin <872912679@qq.com>
2021-12-27 23:08:28 +0800

    q56_I_数组中数字出现的次数I,修改注释

--- src/剑指Offer/第二版/第6章_面试中的各项能力/q56_I_数组中数字出现的次数I/Solution.java
+++ src/剑指Offer/第二版/第6章_面试中的各项能力/q56_I_数组中数字出现的次数I/Solution.java
@@ -1,35 +1,54 @@
 package 剑指Offer.第二版.第6章_面试中的各项能力.q56_I_数组中数字出现的次数I;
 
+import java.util.Arrays;
+
 public class Solution {
 
+    /**
+     * 一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。
+     * 要求时间复杂度是O(n)，空间复杂度是O(1)。
+     */
     public int[] singleNumbers(int[] nums) {
         int a = 0, b = 0;
-        // 第一次遍历,a = 数1^数2
-        for (int cur : nums) {
-            a ^= cur;
+        //  0与任何数异或都为任何数本身
+        // 第一次遍历，a存那两个数字异或的结果，因为两数不相同，a≠0
+        for (int num : nums) {
+            a ^= num;
         }
-        // 找到a中第一个不相同的二进制位=数1和数2不相同的一个二进制位
-        // 由于是十进制表示，所以rightOne 是2^n的表示形式
+        // 找到a中最右边的1，二次遍历划分数组
         int rightOne = a & (~a + 1);
-        a = 0;// 重置a为0，便于下面^操作
+        // 重置a为0，便于下面^操作
+        a = 0;
         // 第二次遍历,利用不相同的二进制位,将原数组分为独立含数1和数2的两个部分
-        for (int cur : nums) {
-            if ((rightOne & cur) == 0) {// 这里只能用！=0或者==0来判断属于哪一个阵营
-                b ^= cur;
+        for (int num : nums) {
+            // 这里只能用!=0或者==0来判断属于哪一个阵营
+            // 因为二进制0和十进制0都是通用，000...0==0
+            if ((rightOne & num) == 0) {
+                b ^= num;
             } else {
-                a ^= cur;
+                a ^= num;
             }
         }
         return new int[]{a, b};
     }
 
-    // 先学习:一个数组中除一个数字外，其余数字出现了两次，找出这个数字
+    /**
+     * 先学习:一个数组中除一个数字外，其余数字出现了两次，找出这个数字
+     */
     public int oneNumbers(int[] nums) {
-        // 0与任何数异或都为任何数本身
+        // 异或两大特性：
+        //   0与任何数异或都为任何数本身
+        //   任何一个数异或它自己都为0
         int res = 0;
         for (int num : nums) {
             res ^= num;
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {1, 2, 5, 2};
+        System.out.println(Arrays.toString(solution.singleNumbers(arr)));
     }
 }
