7205ed85e1a0832cf1d6b472b7188e1e9ae6998f
songshenglin <872912679@qq.com>
2021-12-27 08:53:06 +0800

    q53_II_缺失的数字，重做

--- src/剑指Offer/第二版/第6章_面试中的各项能力/q53_II_缺失的数字/Solution.java
+++ src/剑指Offer/第二版/第6章_面试中的各项能力/q53_II_缺失的数字/Solution.java
@@ -1,18 +1,44 @@
 package 剑指Offer.第二版.第6章_面试中的各项能力.q53_II_缺失的数字;
 
+/**
+ * @author SongShengLin
+ * @date 2021/12/27 8:40 AM
+ * @description
+ */
 public class Solution {
-    // 法:遍历数组
-    public int missingNumber1(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            // 情况1:数组元素与下标对应完整,比如:[0]返回1,[0,1]返回2,[0,1,2]返回3
-            if (nums[i] == i && i == nums.length - 1) {
-                return nums.length;
-            }
-            // 情况2:数组元素与下标对应不完整,比如:[0,1,3]返回2
-            if (nums[i] != i) {
-                return nums[i] - 1;
+    /**
+     * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+     * 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+     */
+    public int missingNumber(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // 中间元素值与下标相等，下一轮查找在右边
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else {
+                // 中间元素与下标不相等
+                // mid==0 或 中间前一个元素与下标相等，mid就是缺失数字
+                if (mid == 0 || nums[mid - 1] == mid - 1) {
+                    return mid;
+                }
+                // 中间元素前一个元素与下标不相等，下一轮遍历左边
+                right = mid - 1;
             }
         }
+        // left来到数组长度位置，说明长度n-1全部匹配，[0,1,2]，返回数组长度
+        if (left == nums.length) {
+            return nums.length;
+        }
+        // 无效的输入，返回-1
         return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 9};
+        System.out.println(solution.missingNumber(arr));
     }
 }
