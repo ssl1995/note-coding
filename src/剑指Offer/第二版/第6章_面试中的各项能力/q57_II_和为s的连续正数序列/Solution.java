d92237a3bba4584ad9260ebf0542b9d9f8eea034
songshenglin <872912679@qq.com>
2021-12-28 09:05:14 +0800

    q57_II_和为s的连续正数序列，重做

--- src/剑指Offer/第二版/第6章_面试中的各项能力/q57_II_和为s的连续正数序列/Solution.java
+++ src/剑指Offer/第二版/第6章_面试中的各项能力/q57_II_和为s的连续正数序列/Solution.java
@@ -5,28 +5,47 @@ import java.util.List;
 
 public class Solution {
 
+    /**
+     * 输入一个正整数 `target` ，输出所有和为 `target` 的连续正整数序列（至少含有两个数）。
+     * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列.1 <= target <= 10^5
+     * 输入：target = 15
+     * 输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+     */
     public int[][] findContinuousSequence(int target) {
-        // 初始化：窗口左右指针，窗口和
-        int left = 1, right = 2, windowsSum = 3;
-        // 根据示例，list中暂存int[]
-        List<int[]> list = new ArrayList<>();
-        while (left < right) {
-            if (windowsSum < target) {// 窗口太小，右移指针再窗口值加大
-                windowsSum += ++right;
-            } else if (windowsSum > target) {// 窗口太大，窗口值减少再左移指针
-                windowsSum -= left++;
-            } else {// 窗口值等于目标值
-                // 获取这一次的temp暂存进list中
-                int[] temp = new int[right - left + 1];
-                for (int i = left; i <= right; i++) {
-                    temp[i - left] = i;
-                }
-                list.add(temp);
-                // 左移窗口
-                windowsSum -= left++;
+        int small = 1, big = 2;
+        // 由于至少要有2个数，small作为起点的上界是不到(target+1)/2
+        int mid = (target + 1) / 2;
+        int curSum = small + big;
+        List<int[]> res = new ArrayList<>();
+        while (small < mid) {
+            if (curSum == target) {
+                listAddData(small, big, res);
             }
+            while (curSum > target && small < mid) {
+                // 在前一个和的基础上进行操作，减少不必要的计算
+                curSum -= small;
+                small++;
+                if (curSum == target) {
+                    listAddData(small, big, res);
+                }
+            }
+
+            big++;
+            curSum += big;
+
         }
         // list转化为二维数组返回
-        return list.toArray(new int[0][]);
+        return res.toArray(new int[0][]);
+    }
+
+    /**
+     * list中添加[small,big]的数组
+     */
+    private void listAddData(int small, int big, List<int[]> list) {
+        int[] temp = new int[big - small + 1];
+        for (int i = small; i <= big; i++) {
+            temp[i - small] = i;
+        }
+        list.add(temp);
     }
 }
