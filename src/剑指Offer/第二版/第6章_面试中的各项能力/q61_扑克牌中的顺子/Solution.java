a47d342a87021c5e243b4f5728d634c8b29ab832
songshenglin <872912679@qq.com>
2021-12-29 00:25:25 +0800

    q61_扑克牌中的顺子，修改注释

--- src/剑指Offer/第二版/第6章_面试中的各项能力/q61_扑克牌中的顺子/Solution.java
+++ src/剑指Offer/第二版/第6章_面试中的各项能力/q61_扑克牌中的顺子/Solution.java
@@ -4,13 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    // 从一幅扑克牌中抽出5张牌,判断是否是顺子
+    /**
+     * 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+     * 2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+     * 输入: nums=[1,2,3,4,5]，就是已经抽出来的5个数字啦
+     * 输出: True
+     */
     public boolean isStraight(int[] nums) {
         // set判断是否有重复的数
         Set<Integer> set = new HashSet<>();
-        // 扑克牌顺子大小从大小王为0,A=1到K=13,范围[0,A,2...10,J,Q,K]
+        // 假设扑克牌中大小王为0,A=1,范围[0,A,2...10,J,Q,K]
         int min = 13, max = 0;
-        // 遍历数组中每一个数字
         for (int num : nums) {
             // 如果是大小王,这一轮循环跳过
             if (num == 0) {
@@ -20,14 +24,13 @@ public class Solution {
                 // 若有重复数字,必不可能构成顺子
                 if (set.contains(num)) {
                     return false;
-                } else {
-                    // 若没有重复数字,找到当前的max,min
-                    // 每一轮必须满足判断顺子的条件:max-min<5
-                    min = Math.min(min, num);
-                    max = Math.max(max, num);
-                    // 当前元素放进set中,给之后的遍历重复元素做铺垫
-                    set.add(num);
                 }
+                // 若没有重复数字,找到当前的max,min
+                // 每一轮必须满足判断顺子的条件:max-min<5
+                min = Math.min(min, num);
+                max = Math.max(max, num);
+                // 当前元素放进set中,给之后的遍历重复元素做铺垫
+                set.add(num);
             }
         }
         // 返回整个数组中的max-min<5的布尔值
