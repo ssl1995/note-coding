97f3a507186b931e61ea2da5c3c9aa33f1eebd99
songshenglin <872912679@qq.com>
2022-01-24 09:21:34 +0800

    LC46全排列、LC48旋转图像

---
+++ src/刷题笔记/力扣/热门100/LC46_全排列/Solution.java
@@ -0,0 +1,63 @@
+package 刷题笔记.力扣.热门100.LC46_全排列;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author SongShengLin
+ * @date 2022/1/24 8:43 AM
+ * @description
+ */
+public class Solution {
+    /**
+     * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。
+     * 你可以 按任意顺序 返回答案。
+     * 输入：nums = [1,2,3]
+     * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        process(nums, 0, res);
+        return res;
+    }
+
+    private void process(int[] nums, int i, List<List<Integer>> res) {
+        if (i == nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for (int num : nums) {
+                temp.add(num);
+            }
+            res.add(temp);
+        } else {
+            // 从i位置往后面临多个选择
+            for (int j = i; j < nums.length; j++) {
+                swap(nums, i, j);
+
+                process(nums, i + 1, res);
+
+                swap(nums, i, j);
+            }
+        }
+    }
+
+
+    private void swap(int[] nums, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> permute = solution.permute(nums);
+        System.out.println(permute);
+    }
+}
---
+++ src/刷题笔记/力扣/热门100/LC48_旋转图像/Solution.java
@@ -0,0 +1,45 @@
+package 刷题笔记.力扣.热门100.LC48_旋转图像;
+
+import java.util.Arrays;
+
+/**
+ * @author SongShengLin
+ * @date 2022/1/24 9:01 AM
+ * @description
+ */
+public class Solution {
+    /**
+     * 全排列
+     * 输入：nums = [1,2,3]
+     * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+     */
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length < 2) {
+            return;
+        }
+        int tR = 0, tC = 0;
+        int dR = matrix.length - 1, dC = matrix[0].length - 1;
+        while (tR < dR) {
+            rotateEdge(matrix, tR++, tC++, dR--, dC--);
+        }
+    }
+
+    private void rotateEdge(int[][] matrix, int tR, int tC, int dR, int dC) {
+        int times = dR - tR;
+        int temp = 0;
+        for (int i = 0; i < times; i++) {
+            temp = matrix[tR][tC + i];
+            matrix[tR][tC + i] = matrix[dR - i][tC];
+            matrix[dR - i][tC] = matrix[dR][dC - i];
+            matrix[dR][dC - i] = matrix[tR + i][dC];
+            matrix[tR + i][dC] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        solution.rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
