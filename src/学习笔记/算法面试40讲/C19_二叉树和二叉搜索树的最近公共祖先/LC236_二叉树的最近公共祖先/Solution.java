e6c12a233eb57fcf3946dec8b42e3e4a4aa7b2c2
songshenglin <872912679@qq.com>
2022-02-17 11:07:01 +0800

    验证二叉树，获取二叉树最近公共祖先

---
+++ src/学习笔记/算法面试40讲/C19_二叉树和二叉搜索树的最近公共祖先/LC236_二叉树的最近公共祖先/Solution.java
@@ -0,0 +1,26 @@
+package 学习笔记.算法面试40讲.C19_二叉树和二叉搜索树的最近公共祖先.LC236_二叉树的最近公共祖先;
+
+import 学习笔记.剑指Offer.utils.TreeNode;
+
+/**
+ * @author SongShengLin
+ * @date 2022/2/17 8:39 AM
+ * @description
+ */
+public class Solution {
+    /**
+     * 二叉树的最近公共祖先
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 成功的base case:最近公共祖先，两者都不为空
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
