ba6b03cfad937a8a0ac50d67f2565ed27e343ae8
songshenglin <872912679@qq.com>
2022-02-19 16:52:16 +0800

    数独两个问题

--- src/学习笔记/算法面试40讲/C32_N皇后问题/LC51_N皇后/Solution.java
+++ src/学习笔记/算法面试40讲/C32_N皇后问题/LC51_N皇后/Solution.java
@@ -35,7 +35,13 @@ public class Solution {
     public void backTrack(char[][] cs, int i, int n) {
         // 终止条件:row越过n-1,说明这是一种正确的放法
         if (i == n) {
-            res.add(char2List(cs));
+            List<String> list = new ArrayList<>();
+            for (char[] c : cs) {
+                // char[]直接转成String，加进list中
+                list.add(String.copyValueOf(c));
+            }
+
+            res.add(list);
             return;
         }
         for (int j = 0; j < n; ++j) {
@@ -51,35 +57,26 @@ public class Solution {
         }
     }
 
-    public boolean isValid(char[][] cs, int row, int col, int n) {
+    public boolean isValid(char[][] cs, int x, int y, int n) {
         // 行不用检查，因为是逐行放入cs中
         // 检查列
         for (int i = 0; i < n; ++i) {
-            if (cs[i][col] == 'Q') {
+            if (cs[i][y] == 'Q') {
                 return false;
             }
         }
         // 检查45°斜线
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+        for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
             if (cs[i][j] == 'Q') {
                 return false;
             }
         }
         // 检查135°反斜线
-        for (int i = row - 1, j = col + 1; i >= 0 && j <= n - 1; i--, j++) {
+        for (int i = x - 1, j = y + 1; i >= 0 && j <= n - 1; i--, j++) {
             if (cs[i][j] == 'Q') {
                 return false;
             }
         }
         return true;
-    }
-
-    public List<String> char2List(char[][] cs) {
-        List<String> list = new ArrayList<>();
-        for (char[] c : cs) {
-            // char[]直接转成String，加进list中
-            list.add(String.copyValueOf(c));
-        }
-        return list;
     }
 }
