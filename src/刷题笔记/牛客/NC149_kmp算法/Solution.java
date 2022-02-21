54328ecc321ccb3d4e0cf79dfa0e026ffbb388ac
songshenglin <872912679@qq.com>
2022-02-21 09:38:01 +0800

    kmp

--- src/刷题笔记/牛客/NC149_kmp算法/Solution.java
+++ src/刷题笔记/牛客/NC149_kmp算法/Solution.java
@@ -1,8 +1,16 @@
 package 刷题笔记.牛客.NC149_kmp算法;
 
+import java.util.Arrays;
+
 public class Solution {
 
-    // 牛客149kmp:给你一个非空模板串S是匹配串，一个文本串T是原串，问匹配串S在原串T中出现了多少次
+    /**
+     * 牛客149kmp:
+     * 给你一个非空模板串S是匹配串，一个文本串T是原串，问匹配串S在原串T中出现了多少次
+     * 输入：
+     * "ababab","abababab"
+     * 返回值：2
+     */
     public int kmp(String S, String T) {
         if (T == null || S == null || S.length() < 1 || T.length() < S.length()) {
             return 0;
@@ -11,7 +19,8 @@ public class Solution {
         char[] s2 = S.toCharArray();
         int i1 = 0;
         int i2 = 0;
-        int[] next = getNext(s2);// 改造1：next数组多算1位，记录整个匹配串的最长前后缀长度
+        // 改造1：next数组多算1位，记录整个匹配串的最长前后缀长度
+        int[] next = getNext(s2);
         int res = 0;
         while (i1 < s1.length && i2 < s2.length) {
             if (s1[i1] == s2[i2]) {
@@ -21,26 +30,33 @@ public class Solution {
                 i1++;
             } else {
                 i2 = next[i2];
-            }// 以上是常规kmp
-            if (i2 == s2.length) { // 改造2:i2到达s2.len,代表匹配上了,结果+1
+            }
+            // 以上是常规kmp
+            // 改造2:i2到达s2.len,代表匹配上了,结果+1
+            if (i2 == s2.length) {
                 res++;
-                i2 = next[i2];// i2再回退到整个s2的最长后缀位置开始二轮匹配
+                // i2再回退到整个s2的最长后缀位置开始二轮匹配
+                i2 = next[i2];
             }
         }
-        return res;// 返回匹配个数
+        return res;
     }
 
-    // 获得match串的改造next数组，比常规next多一位数，多出的末尾数组记录整个m的最长前后缀长度
+    /**
+     * 获得match串的改造next数组，比常规next多一位数，多出的末尾数组记录整个m的最长前后缀长度
+     */
     private int[] getNext(char[] match) {
         if (match.length == 1) {
             return new int[]{-1};
         }
-        int[] next = new int[match.length + 1];  // next改造1：next多算一个,多出的末尾数组记录整个ms的最长前后缀长度
+        // next改造1：next多算一个,多出的末尾数组记录整个ms的最长前后缀长度
+        int[] next = new int[match.length + 1];
         next[0] = -1;
         next[1] = 0;
         int i = 2;
         int cn = 0;
-        while (i < match.length + 1) {  // next改造2：i < match.length + 1
+        // next改造2：i < match.length + 1
+        while (i < match.length + 1) {
             if (match[i - 1] == match[cn]) {
                 next[i++] = ++cn;
             } else if (cn > 0) {
@@ -58,5 +74,7 @@ public class Solution {
         // 返回match子串在str中匹配的起始位置
         Solution solution = new Solution();
         System.out.println(solution.kmp(S, T));
+        // next数组：[-1, 0, 0, 1, 0, 1, 2, 3, 2]
+        System.out.println("next数组：" + Arrays.toString(solution.getNext(T.toCharArray())));
     }
 }
--- src/学习笔记/牛客算法课/基础班提升/课2_并查集和KMP/Q03_KMP/KMP.java
+++ src/学习笔记/牛客算法课/基础班提升/课2_并查集和KMP/Q03_KMP/KMP.java
@@ -1,10 +1,15 @@
 package 学习笔记.牛客算法课.基础班提升.课2_并查集和KMP.Q03_KMP;
 
 
+import java.util.Arrays;
+
 public class KMP {
-    // KMP:字符串s和m，s是否包含m，如果包含返回m在s中开始的位置?再结合NC149_kmp算法做比较
-    // 要求整体时间复杂度O(N),N是s1的长度，M是s2的长度
-    public static int getIndexOf(String s, String m) {
+    /**
+     * KMP:字符串s和m，s是否包含m，如果包含返回m在s中开始的位置?
+     * 再结合NC149_kmp算法做比较
+     * 要求整体时间复杂度O(N),N是s1的长度，M是s2的长度
+     */
+    public static int kmp(String s, String m) {
         if (s == null || m == null || m.length() < 1 || s.length() < m.length()) {
             return -1;
         }
@@ -12,14 +17,18 @@ public class KMP {
         char[] cs2 = m.toCharArray();
         int i1 = 0;
         int i2 = 0;
-        int[] next = getNextArray(cs2);// 获得待匹配串的next数组
+        // 获得待匹配串的next数组
+        int[] next = getNextArray(cs2);
         while (i1 < cs1.length && i2 < cs2.length) {
-            if (cs1[i1] == cs2[i2]) {// i1与i2都匹配成功，与暴力法一样都后移指针
+            // i1与i2都匹配成功，与暴力法一样都后移指针
+            if (cs1[i1] == cs2[i2]) {
                 i1++;
                 i2++;
-            } else if (next[i2] == -1) {// m串的遍历指针来到next[i2]=-1的位置，表示无法加速匹配，只能暴力移动i1
+            } else if (next[i2] == -1) {
+                // m串的遍历指针来到next[i2]=-1的位置，表示无法加速匹配，只能暴力移动i1
                 i1++;
-            } else {// 只要不是-1,i2就加速移动到最长后缀下一个位置,由于数组下标是0开始,所以i2移动到next[i2]下标
+            } else {
+                // 只要不是-1,i2就加速移动到最长后缀下一个位置,由于数组下标是0开始,所以i2移动到next[i2]下标
                 i2 = next[i2];
             }
         }
@@ -27,27 +36,36 @@ public class KMP {
         return i2 == cs2.length ? i1 - i2 : -1;
     }
 
-    // next数组：每个位置前字符串的前后缀最大匹配长度
+    /**
+     * next数组：每个位置i前面字符串的前后缀最大匹配长度
+     * 不能取到前面的整体
+     */
     public static int[] getNextArray(char[] match) {
         // 长度为1的数组,前面没有元素,规定next值为-1
         if (match.length == 1) {
             return new int[]{-1};
         }
         int[] next = new int[match.length];
-        next[0] = -1; // 第一个位置前面没有元素，规定为-1
-        next[1] = 0;// 第二个位置前面1个元素，匹配个数为1
-        int i = 2;// 遍历指针从第3个元素开始
+        // 规定：第一个位置前面没有元素，规定匹配长度为-1
+        next[0] = -1;
+        // 规定：第二个位置前面1个元素，但是不能取整体，规定匹配长度为0
+        next[1] = 0;
+        int i = 2;
+        // 遍历指针从第3个元素开始
         // cn两个含义:
         // 1.拿哪个位置的字符跟i-1比,由于i初始化2,i-1是1,所以cn=0
         // 2.i位置的前缀和后缀最大匹配长度=最大匹配下标+1的值
         int cn = 0;
         while (i < match.length) {
-            if (match[i - 1] == match[cn]) {// i-1位置和cn相同，next[i]=cn+1,cn和i后移再匹配
+            // i-1位置和cn相同，next[i]=cn+1,cn和i后移再匹配
+            if (match[i - 1] == match[cn]) {
                 next[i++] = ++cn;
-            } else if (cn > 0) {// i-1位置和cn位置不相同：有两种情况
+            } else if (cn > 0) {
+                // i-1位置和cn位置不相同：有两种情况
                 // 情况1：cn一直往前跳,但cn必须大于数组0位置
                 cn = next[cn];
-            } else {//  情况2：cn一直往前跳来到0位置，说明match[i]无前后缀匹配，next[i++]=0
+            } else {
+                //  情况2：cn一直往前跳来到0位置，说明match[i]无前后缀匹配，next[i++]=0
                 next[i++] = 0;
             }
         }
@@ -57,8 +75,12 @@ public class KMP {
     public static void main(String[] args) {
         String str = "abcabcababaccc";
         String match = "ababa";
-        // next = [-1, 0, 0, 1, 2]
-        // System.out.println(Arrays.toString(getNextArray(match.toCharArray())));
-        System.out.println(getIndexOf(str, match));// 返回匹配串在str中开始的位置
+        System.out.println(kmp(str, match));
+        // next数组：[-1, 0, 0, 1, 2]
+        System.out.println("next数组：" + Arrays.toString(getNextArray(match.toCharArray())));
+        System.out.println("------");
+        String test = "aabaaba";
+        // next数组：[-1, 0, 1, 0, 1, 2, 3]
+        System.out.println("next数组：" + Arrays.toString(getNextArray(test.toCharArray())));
     }
 }
