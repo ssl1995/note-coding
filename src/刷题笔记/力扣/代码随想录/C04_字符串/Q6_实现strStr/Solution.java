b07bcb0cba323696eedc0d0ee94b4437ac169941
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-11 10:30:05 +0800

    左旋转字符串

--- src/刷题笔记/力扣/代码随想录/C04_字符串/Q6_实现strStr/Solution.java
+++ src/刷题笔记/力扣/代码随想录/C04_字符串/Q6_实现strStr/Solution.java
@@ -6,4 +6,14 @@ package 刷题笔记.力扣.代码随想录.C04_字符串.Q6_实现strStr;
  * @Describe:
  */
 public class Solution {
+
+    /**
+     * LC28 实现strStr
+     * 输入：haystack = "hello", needle = "ll"
+     * 输出：2
+     * 注意：空串返回0
+     */
+    public int strStr(String haystack, String needle) {
+        return 0;
+    }
 }
