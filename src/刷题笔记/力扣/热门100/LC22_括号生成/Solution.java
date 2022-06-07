e0737876009d6f69646aa2038b8ee264f3ce1fe6
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-06-07 09:53:07 +0800

    力扣top100：LC22_括号生成

--- src/刷题笔记/力扣/热门100/LC22_括号生成/Solution.java
+++ src/刷题笔记/力扣/热门100/LC22_括号生成/Solution.java
@@ -17,10 +17,19 @@ public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
         // n对括号=会生成2n长的字符串，需要左括号数=右括号数=n
+        // 比如：n=3，会生成6个括号，左右括号数都是n=3
         recursion(n, n, "", res);
         return res;
     }
 
+    /**
+     * 递归生成n对有效的括号
+     *
+     * @param leftNeed  还有多少个左括号需要生成
+     * @param rightNeed 还有多少个右括号需要生成
+     * @param sub       当期生成的有效括号
+     * @param res       结果集
+     */
     private void recursion(int leftNeed, int rightNeed, String sub, List<String> res) {
         if (leftNeed == 0 && rightNeed == 0) {
             res.add(sub);
