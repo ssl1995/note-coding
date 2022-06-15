63785a011b6ff0214883a31ec88c7e74554fbb80
songshenglin <872912679@qq.com>
2022-06-15 23:51:23 +0800

    力扣Top100：LC128_最长连续序列

--- src/刷题笔记/力扣/剑指Offer/第三版_专项突击版/第15章_图/q119_最长连续序列/Solution1.java
+++ src/刷题笔记/力扣/剑指Offer/第三版_专项突击版/第15章_图/q119_最长连续序列/Solution1.java
@@ -11,19 +11,25 @@ import java.util.Set;
  * @description
  */
 public class Solution1 {
-    // 用并查集
+    /**
+     * nums = [100,4,200,1,3,2]
+     */
     public int longestConsecutive(int[] nums) {
         // father[i]:记录每个整数所在子集的父节点
         Map<Integer, Integer> fathers = new HashMap<>();
         // counts[i]:i结点为根结点的子集整数数量
         Map<Integer, Integer> counts = new HashMap<>();
+
         Set<Integer> all = new HashSet<>();
+
+        // 初始化
         for (int num : nums) {
             fathers.put(num, num);
             counts.put(num, 1);
             all.add(num);
         }
 
+        // 是否包含相邻元素
         for (int num : nums) {
             if (all.contains(num + 1)) {
                 union(fathers, counts, num, num + 1);
@@ -45,11 +51,13 @@ public class Solution1 {
         int fatherOfJ = findFather(fathers, j);
         // 两个父节点不相同，就合并两个子图
         if (fatherOfI != fatherOfJ) {
+            // i挂着j名下
             fathers.put(fatherOfI, fatherOfJ);
 
             int countOfI = counts.get(fatherOfI);
             int countOfJ = counts.get(fatherOfJ);
 
+            // j名下数量更新
             counts.put(fatherOfJ, countOfI + countOfJ);
         }
     }
--- src/刷题笔记/力扣/热门100/LC128_最长连续序列/Solution.java
+++ src/刷题笔记/力扣/热门100/LC128_最长连续序列/Solution.java
@@ -1,8 +1,8 @@
 package 刷题笔记.力扣.热门100.LC128_最长连续序列;
 
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -21,58 +21,64 @@ public class Solution {
      * 如下的方法会超时
      */
     public int longestConsecutive(int[] nums) {
-        if (nums == null) {
-            return 0;
-        }
-        if (nums.length == 1) {
-            return 1;
-        }
-        Set<Integer> set = new HashSet<>();
+        // father[i]:记录每个整数所在子集的父节点
+        Map<Integer, Integer> fathers = new HashMap<>();
+        // counts[i]:i结点为根结点的子集整数数量
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        Set<Integer> all = new HashSet<>();
+
+        // 初始化
         for (int num : nums) {
-            set.add(num);
+            fathers.put(num, num);
+            counts.put(num, 1);
+            all.add(num);
         }
 
-        int maxLen = 0;
-        while (!set.isEmpty()) {
-            Integer num = set.iterator().next();
-
-            int len = dfs(set, num);
-
-            maxLen = Math.max(maxLen, len);
-        }
-
-        return maxLen;
-    }
-
-    private int dfs(Set<Integer> set, int num) {
-        LinkedList<Integer> queue = new LinkedList<>();
-        queue.offer(num);
-        set.remove(num);
-
-        int len = 1;
-
-        while (!queue.isEmpty()) {
-            Integer poll = queue.poll();
-
-            int[] temp = new int[]{poll - 1, poll + 1};
-
-            for (int i = 0; i < 2; i++) {
-
-                if (set.contains(temp[i])) {
-                    queue.offer(temp[i]);
-                    set.remove(temp[i]);
-
-                    len++;
-                }
+        // 是否包含相邻元素
+        for (int num : nums) {
+            if (all.contains(num + 1)) {
+                union(fathers, counts, num, num + 1);
+            }
+            if (all.contains(num - 1)) {
+                union(fathers, counts, num, num - 1);
             }
         }
-        return len;
 
+        int longest = 0;
+        for (int length : counts.values()) {
+            longest = Math.max(longest, length);
+        }
+        return longest;
     }
+
+    private void union(Map<Integer, Integer> fathers, Map<Integer, Integer> counts, int i, int j) {
+        int fatherOfI = findFather(fathers, i);
+        int fatherOfJ = findFather(fathers, j);
+        // 两个父节点不相同，就合并两个子图
+        if (fatherOfI != fatherOfJ) {
+            // i挂着j名下
+            fathers.put(fatherOfI, fatherOfJ);
+
+            int countOfI = counts.get(fatherOfI);
+            int countOfJ = counts.get(fatherOfJ);
+
+            // j名下数量更新
+            counts.put(fatherOfJ, countOfI + countOfJ);
+        }
+    }
+
+    private int findFather(Map<Integer, Integer> fathers, int i) {
+        if (fathers.get(i) != i) {
+            fathers.put(i, findFather(fathers, fathers.get(i)));
+        }
+        return fathers.get(i);
+    }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums ={};
+        int[] nums = {};
 
         System.out.println(solution.longestConsecutive(nums));
     }
