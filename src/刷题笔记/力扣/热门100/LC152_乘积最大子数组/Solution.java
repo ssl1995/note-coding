5ad1cc57d2bbb87271f0f85776b2977227fdbd10
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-06-17 10:23:35 +0800

    Top100:LC152_乘积最大子数组

--- src/刷题笔记/力扣/热门100/LC152_乘积最大子数组/Solution.java
+++ src/刷题笔记/力扣/热门100/LC152_乘积最大子数组/Solution.java
@@ -14,27 +14,34 @@ public class Solution {
      * 输出: 6
      */
     public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE;
         // 由于存在负数,导致子数组乘积从最大变成最小
         // 所以每一位数组元素都需要存当前位置的最大乘积、最小乘积
         // iMax:表示0到i的最大乘积
         int iMax = 1;
         // iMin:表示0到i的最小乘积
         int iMin = 1;
-        for (int i = 0; i < nums.length; i++) {
+
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
             // 遇到负数，交换iMax、iMin
-            if (nums[i] < 0) {
+            if (num < 0) {
                 int temp = iMax;
                 iMax = iMin;
                 iMin = temp;
             }
 
-            iMax = Math.max(iMax * nums[i], nums[i]);
-            iMin = Math.min(iMin * nums[i], nums[i]);
+            iMax = Math.max(iMax * num, num);
+            iMin = Math.min(iMin * num, num);
 
             max = Math.max(max, iMax);
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+        Solution solution = new Solution();
+        System.out.println(solution.maxProduct(nums));
     }
 
 }
