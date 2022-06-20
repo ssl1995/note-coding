1d06aa11bf1e448919c256953867f5e38a487b30
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-06-20 10:42:38 +0800

    Top100:LC300_最长递增子序列

--- src/刷题笔记/力扣/热门100/LC300_最长递增子序列/Solution.java
+++ src/刷题笔记/力扣/热门100/LC300_最长递增子序列/Solution.java
@@ -16,13 +16,16 @@ public class Solution {
         }
         // dp[i]：[0..i]组成的最长子序列长度，初始化为1
         int[] dp = new int[nums.length];
+        // 初试化：最长连续子序列长度最低为1
         Arrays.fill(dp, 1);
-        // 最长连续子序列长度最低为1
+
         int manLen = 1;
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j <= i; j++) {
                 if (nums[j] < nums[i]) {
+                    // 动态规划
                     dp[i] = Math.max(dp[i], dp[j] + 1);
+
                     manLen = Math.max(dp[i], manLen);
                 }
             }
