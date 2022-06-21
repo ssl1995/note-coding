bac5310b4cbd19e91f338433b296507b23b03ffa
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-06-21 10:09:53 +0800

    Top100:LC399_除法求值-放弃，以后再做

--- src/刷题笔记/力扣/热门100/LC399_除法求值/Solution.java
+++ src/刷题笔记/力扣/热门100/LC399_除法求值/Solution.java
@@ -1,5 +1,19 @@
 package 刷题笔记.力扣.热门100.LC399_除法求值;
 
-public class Solution {
+import java.util.List;
 
+public class Solution {
+    /**
+     * 除法求值
+     * 输入：equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+     * 输出：[6.00000,0.50000,-1.00000,1.00000,-1.00000]
+     * 解释：
+     * 条件：a / b = 2.0, b / c = 3.0
+     * 问题：a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+     * 结果：[6.0, 0.5, -1.0, 1.0, -1.0 ]
+     */
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        // 放弃，以后再做
+        return null;
+    }
 }
