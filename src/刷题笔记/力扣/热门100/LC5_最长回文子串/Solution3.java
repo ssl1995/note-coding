d5252eb34c0d905d556876e93bfe22db66aec3b9
songshenglin <872912679@qq.com>
2022-07-10 15:18:44 +0800

    LC5_最长回文子串

--- src/刷题笔记/力扣/热门100/LC5_最长回文子串/Solution3.java
+++ src/刷题笔记/力扣/热门100/LC5_最长回文子串/Solution3.java
@@ -13,36 +13,32 @@ public class Solution3 {
         if (s.length() < 2) {
             return s;
         }
-        int len = s.length();
-        int maxLen = 1;
-        int begin = 0;
-        char[] cs = s.toCharArray();
-        // dp[i][j]:表示s从i..j位置是否是回文串
-        boolean[][] dp = new boolean[len][len];
-        // 初始化：对角线上单独一个字符肯定是回文子串
-        for (int i = 0; i < len; i++) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
             dp[i][i] = true;
         }
-        // 外层:枚举所有子串长度，从1开始
-        for (int j = 1; j < len; j++) {
-            // 内层:从(0,j]判断该区域内回文情况
-            for (int i = 0; i < j; i++) {
-                // 首尾不相等时，必不是回文串
-                if (cs[i] != cs[j]) {
-                    dp[i][j] = false;
+
+        int maxLen = 1;
+        int begin = 0;
+        // [j...i]
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[j][i] = i - j + 1 <= 3 || dp[j + 1][i - 1];
                 } else {
-                    // 首尾相等时，有2种情况
-                    // 情况1：s[i...j]长度不超过3，不用检查必为回文串
-                    // 情况2：s[i...j]长度大于3，由s[i+1...j-1]来判断
-                    dp[i][j] = j - i + 1 <= 3 || dp[i + 1][j - 1];
+                    dp[j][i] = false;
                 }
-                // 更新max和begin
-                if (dp[i][j] && j - i + 1 > maxLen) {
-                    maxLen = j - i + 1;
-                    begin = i;
+
+                if (dp[j][i] && i - j + 1 > maxLen) {
+                    maxLen = i - j + 1;
+                    // 内层循环时j，初始位置是j
+                    begin = j;
                 }
             }
         }
+        // substring第二个参数是begin+len;不是len
         return s.substring(begin, begin + maxLen);
     }
 
