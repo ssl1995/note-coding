7f40fb63ab5d8181e012f0245af17faef4ec4d4b
songshenglin <872912679@qq.com>
2022-07-17 10:43:21 +0800

    q06_从尾到头打印链表

--- src/刷题笔记/力扣/剑指Offer/第二版_经典版/第2章_面试需要的基础知识/q06_从尾到头打印链表/Solution.java
+++ src/刷题笔记/力扣/剑指Offer/第二版_经典版/第2章_面试需要的基础知识/q06_从尾到头打印链表/Solution.java
@@ -3,16 +3,19 @@ package 刷题笔记.力扣.剑指Offer.第二版_经典版.第2章_面试需要
 
 import 刷题笔记.力扣.utils.ListNode;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class Solution {
 
     /**
-     * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）
+     * 题目：输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+     * 示例：
+     * 输入：head = [1,3,2]
+     * 输出：[2,3,1]
      */
     public int[] reversePrint(ListNode head) {
-        // LinkedList继承了队列、栈,可以使用栈的push,pop的API
-        LinkedList<Integer> stack = new LinkedList<>();
+        Deque<Integer> stack = new LinkedList<>();
         while (head != null) {
             stack.push(head.val);
             head = head.next;
