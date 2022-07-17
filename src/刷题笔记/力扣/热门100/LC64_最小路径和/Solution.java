e15fbf365804fbcbd098795e183e78a8d154fdc1
songshenglin <872912679@qq.com>
2022-07-17 10:27:48 +0800

    LC64_最小路径和

---
+++ src/刷题笔记/力扣/热门100/LC64_最小路径和/Solution.java
@@ -0,0 +1,39 @@
+package 刷题笔记.力扣.热门100.LC64_最小路径和;
+
+/**
+ * @author SongShengLin
+ * @date 2022/7/17 10:11
+ * @description
+ */
+public class Solution {
+    /**
+     * 题目：给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+     * 说明：每次只能向下或者向右移动一步。
+     */
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                // 最小路径：取min
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
