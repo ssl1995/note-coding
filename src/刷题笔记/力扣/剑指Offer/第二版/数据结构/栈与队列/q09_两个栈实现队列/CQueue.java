7f3d06d555daa8c3c034d925f50ae6d9c74433da
songshenglin <872912679@qq.com>
2022-07-21 09:04:18 +0800

    q09_两个栈实现队列

--- src/刷题笔记/力扣/剑指Offer/第二版/数据结构/栈与队列/q09_两个栈实现队列/CQueue.java
+++ src/刷题笔记/力扣/剑指Offer/第二版/数据结构/栈与队列/q09_两个栈实现队列/CQueue.java
@@ -12,21 +12,32 @@ public class CQueue {
         stack2 = new LinkedList<>();
     }
 
+    /**
+     * 队列尾部添加元素
+     */
     public void appendTail(int value) {
         // 往队列中添加元素，只用往栈1中添加即可
         stack1.push(value);
     }
 
+    /**
+     * 队列头部删除元素
+     */
     public int deleteHead() {
         if (stack1.isEmpty() && stack2.isEmpty()) {
             return -1;
         }
         // 队列出队，栈2为空就要往里面倒入元素保证先进先出
-        if (stack2.isEmpty()) {
+        pushToStack2();
+        // 返回栈2的栈顶元素
+        return stack2.pop();
+    }
+
+    private void pushToStack2() {
+        while (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
-        return stack2.pop();
     }
 }
